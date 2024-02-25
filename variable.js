fb67334b2dfbf242a1f49177b6f2a6480cd27d1c
hina naseer <99113701+hinanaseer@users.noreply.github.com>
2024-02-25 07:16:29 +0500

    add variable.ts file

---
+++ variable.js
@@ -0,0 +1,8 @@
+var uname = "Suresh Dasari";
+var ulocation = "Hyderabad";
+var marks = 400;
+var percentage = 45.6;
+console.log("Name:" + uname);
+console.log("Location:" + ulocation);
+console.log("Marks:" + marks);
+console.log("Percentage:" + percentage);
---
+++ variable.ts
@@ -0,0 +1,18 @@
+
+var uname = "Suresh Dasari";
+
+var ulocation: string = "Hyderabad";
+
+var marks = 400;
+
+var percentage: number = 45.6;
+
+console.log("Name:" + uname);
+
+console.log("Location:" + ulocation);
+
+console.log("Marks:" + marks);
+
+console.log("Percentage:" + percentage);
+
+
